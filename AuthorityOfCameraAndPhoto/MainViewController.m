bb144d7ed8500b7abb16cee230a737f975662a19
陈宇峰 <yufengcode@gmail.com>
2016-06-20 13:47:26 +0800

    UI重构

---
+++ AuthorityOfCameraAndPhoto/MainViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  AuthorityOfCameraAndPhoto
+//
+//  Created by chenyufeng on 6/20/16.
+//  Copyright © 2016 chenyufengweb. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.title = @"相机照片授权";
+}
+@end
